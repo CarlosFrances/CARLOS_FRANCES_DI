77861ade995ba33eb35a0f5377df3e8ac1c873ad
CarlosFrances <113194702+CarlosFrances@users.noreply.github.com>
2023-03-02 08:44:29 +0100

    u

--- JavaScript/proyectosAngular/T7/directivas/src/app/app.module.ts
+++ JavaScript/proyectosAngular/T7/directivas/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectivaifComponent } from './components/directivaif/directivaif.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DirectivaifComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
